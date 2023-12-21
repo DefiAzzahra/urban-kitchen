7842c58e1b3779776b6ef111f033a67da48864d1
DefiAzzahra <150224763+DefiAzzahra@users.noreply.github.com>
2023-12-21 16:15:05 +0700

    Update cardMakanan.tsx
    tidy up

--- src/app/komponen/cardMakanan.tsx
+++ src/app/komponen/cardMakanan.tsx
@@ -29,15 +29,16 @@ function CardMakanan() {
                 return (
                     // eslint-disable-next-line react/jsx-key
                     <div>{""}
-                        <div className='' ref={makanan.Link} >
+                        <div className='' ref={makan.Link} >
 
 
 
-                            <div className='pl-20 pr-4'>
-                                <img src={makan.gambar} className="md:h-40 ms:h-32 h-24 md:w-44 ms:w-32 w-24  md:ml-2 ms:ml-6 ml-4 md:mt-3 ms:mt-2 mt-1 md:translate-y-[80px] ms:translate-y-[30px] z-20 " />
-                                <div className='bg-inherit border-solid border-[3px] border-red-900 rounded-[30px] md:w-full ms:w-80 w-76 md:h-64 ms:h-54 h-48 translate-y-[50px]'>
-                                    <h1 className='text-center pt-[100px] font-bold md:text-xl ms:text-l text-m'>{makan.nama}</h1>
-                                    <p className='text-center pt-3 font-italic '>{makan.keterangan}</p>
+                            <div className='pl-20 pr-4 relative'>
+                                <div className='bg-inherit border-solid border-[3px] border-red-900 z-10 rounded-[30px] md:w-full ms:w-80 w-76 md:h-64 ms:h-54 h-48 translate-y-[200px] mb-52'>
+                                    <img src={makan.gambar} className="md:h-40 z-30 ms:h-32 h-24 md:w-44 ms:w-32 w-24  md:ml-2 ms:ml-6 ml-4 md:mt-3 ms:mt-2 mt-1 -translate-y-[100px]  z-20 " />
+                                    <h1 className='text-center pt-[100px] font-bold md:text-xl ms:text-l text-m -translate-y-[200px]'>{makan.nama}</h1>
+                                    <p className='text-center pt-5 font-italic
+                                     -translate-y-[200px] '>{makan.keterangan}</p>
                                 </div>
                             </div>
 
