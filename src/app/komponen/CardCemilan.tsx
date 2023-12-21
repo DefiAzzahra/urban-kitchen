b07bb061e8ed84f3d71e2049d05190a72394355f
DefiAzzahra <150224763+DefiAzzahra@users.noreply.github.com>
2023-12-21 16:15:21 +0700

    Update CardCemilan.tsx
    tidy up

--- src/app/komponen/CardCemilan.tsx
+++ src/app/komponen/CardCemilan.tsx
@@ -35,10 +35,11 @@ function CardCemilan() {
 
 
                             <div className='pl-20 pr-4 z-10'>
-                                <img src={cemilan.gambar} className="md:h-40 ms:h-32 h-24 md:w-44 ms:w-32 w-24  md:ml-2 ms:ml-6 ml-4 md:mt-3 ms:mt-2 mt-1 md:translate-y-[80px] ms:translate-y-[30px] translate-y-[25px]" />
-                                <div className='bg-inherit border-solid border-[3px] border-red-900 rounded-[30px] md:w-full ms:w-80 w-76 md:h-64 ms:h-54 h-48 translate-y-[25px]'>
-                                    <h1 className='text-center pt-20 font-bold md:text-xl ms:text-l text-m'>{cemilan.nama}</h1>
-                                    <p className='text-center pt-3 font-italic '>{cemilan.keterangan}</p>
+                                <div className='bg-inherit border-solid border-[3px] border-red-900 z-10 rounded-[30px] md:w-full ms:w-80 w-76 md:h-64 ms:h-54 h-48 translate-y-[200px] mb-52'>
+                                    <img src={cemilan.gambar} className="md:h-40 z-30 ms:h-32 h-24 md:w-44 ms:w-32 w-24  md:ml-2 ms:ml-6 ml-4 md:mt-3 ms:mt-2 mt-1 -translate-y-[100px]  z-20 " />
+                                    <h1 className='text-center pt-[120px] font-bold md:text-xl ms:text-l text-m -translate-y-[200px]'>{cemilan.nama}</h1>
+                                    <p className='text-center pt-5 font-italic
+                                     -translate-y-[200px] '>{cemilan.keterangan}</p>
                                 </div>
                             </div>
 
